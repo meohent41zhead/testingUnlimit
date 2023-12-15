361b210426bb714ab1f8ea27a3e4b5dc28fa5220
Neko-chan #5040 <meohexa.1.a@gmail.com>
2023-12-15 20:23:37 +0700

    ?
    ?

---
+++ src/example/main.java
@@ -0,0 +1,11 @@
+package example
+
+import arc.*;
+
+public class main extends Plugin{
+
+    @Override
+    public void init(){
+
+    }
+}
---
+++ src/example/service.java
@@ -0,0 +1,18 @@
+package example
+
+import java.util.*;
+import arc.*;
+
+public class service{
+    public class playerData(){
+        public String playerUuid;
+        public String playerName;
+
+        public playerData(String name, String uid) {
+            this.playerName = name;
+            this.playerUuid = uid;
+        }
+
+        // service in here
+    }
+}
