3e6c4c368badaeb68ed834bafda7fd093e62dc48
Neko-chan #5040 <80139315+meohent41zhead@users.noreply.github.com>
2023-12-17 19:59:33 +0700

    Update service.java

--- src/example/service.java
+++ src/example/service.java
@@ -2,6 +2,7 @@ package example
 
 import java.util.*;
 import arc.*;
+import arc.util.Log.*;
 
 public class service{
     public class playerData(){
@@ -14,5 +15,37 @@ public class service{
         }
 
         // service in here
+    }
+
+    public class actionFramework{
+        public String task;
+
+        public actionFramework(){
+            this.task = "empty";
+        }
+    }
+
+    public class databaseController{
+
+        public databaseController()
+    }
+
+    public class databaseDataType{
+        public String dataType;
+
+        public databaseDataType(String type){
+            switch (type) {
+                case "playerData":
+                    this.dataType = "playerData";
+                case "pd":
+                    this.dataType = "playerData";
+                case "pdata":
+                    this.dataType = "playerData";
+
+                default :
+                    this.dataType = "undefined";
+                    Log.err("Register database dt not have any case correct. Please check");
+            }
+        }
     }
 }
